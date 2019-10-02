892ab61c308711ccb071aa36173fdf987739df43
Jeroen Roovers <jroovers93@gmail.com>
2019-10-02 18:47:56 +0200

    Exercise 4 finished: two-way binding and pipes

--- workshop/app/app.module.ts
+++ workshop/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent,
-    ProductListComponent],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- workshop/app/products/product-list.component.ts
+++ workshop/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 1,
@@ -30,4 +33,9 @@ export class ProductListComponent {
                 "https://openclipart.org/image/300px/svg_to_png/213924/python1.png"
         }
     ];
+    listFilter: string = 'programming';
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
