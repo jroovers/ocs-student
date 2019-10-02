d008014950f568669083f000a2448c6abcb0e045
Jeroen Roovers <jroovers93@gmail.com>
2019-10-02 17:05:46 +0200

    Finished exercise 2: Components

---
+++ workshop/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-app',
+    styleUrls: ['/app.component.css'],
+    template: `
+        <div><h1>{{pageTitle}}</h1>
+            <div>My First Component</div>
+        </div>
+        `
+})
+export class AppComponent {
+    pageTitle: string = `PRG Product Management`;
+}
