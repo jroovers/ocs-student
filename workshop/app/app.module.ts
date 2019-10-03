ebc05c362a5d89b357c7ec61922e3a9e56727cb8
Jeroen Roovers <jroovers93@gmail.com>
2019-10-03 16:46:57 +0200

    Exercise 11: modules

--- workshop/app/app.module.ts
+++ workshop/app/app.module.ts
@@ -1,35 +1,27 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from "./app.component";
-import { ProductDetailComponent } from './products/product-detail.component';
-import { ProductDetailGuard } from './products/product-guard.service';
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { ProductListComponent } from "./products/product-list.component";
-import { StarComponent } from './shared/star.component';
+import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
-    AppRoutingModule
+	RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ProductFilterPipe,
-    WelcomeComponent,
-    ProductDetailComponent,
-    StarComponent
+    WelcomeComponent
   ],
-  providers: [ProductDetailGuard],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- workshop/app/products/product-list.component.ts
+++ workshop/app/products/product-list.component.ts
@@ -1,38 +1,34 @@
-import { Component, OnInit } from "@angular/core";
-import { IProduct } from "./product";
-import { ProductService } from "../services/product.service";
-import { Json } from "@angular/core/src/facade/lang";
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
+import { ProductService } from '../services/product.service';
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css'],
-    moduleId: module.id
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = '';
-    products: IProduct[];
-    errorMessage: any;
+    listFilter: string;
+    errorMessage: string;
 
-    /**
-     *
-     */
+    products: IProduct[];
+
     constructor(private _productService: ProductService) {
 
     }
 
-    ngOnInit(): void {
-        console.log('In OnInit');
-        this._productService.getProducts()
-        .subscribe(products => this.products = products, error => this.errorMessage = <any>error)
-    }
-
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        this._productService.getProducts()
+                .subscribe(products => this.products = products,
+                           error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void {
