d2bf12d53e0f88e797488770e382215ce609e5e6
Jeroen Roovers <jroovers93@gmail.com>
2019-10-03 13:56:46 +0200

    Exercise 9 fixes

--- workshop/app/app.module.ts
+++ workshop/app/app.module.ts
@@ -6,17 +6,23 @@ import { FormsModule } from "@angular/forms";
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductFilterPipe,
+    WelcomeComponent,
+    ProductDetailComponent,
     StarComponent
   ],
   bootstrap: [AppComponent]
