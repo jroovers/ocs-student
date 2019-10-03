fecaa3122dfdf931a9ca9efb957d4cf15dd7592b
Jeroen Roovers <jroovers93@gmail.com>
2019-10-03 14:06:05 +0200

    Exercise 10 step 1
    added route to product details

--- workshop/app/app-routing.module.ts
+++ workshop/app/app-routing.module.ts
@@ -1,20 +1,22 @@
-      
+
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProductListComponent } from './products/product-list.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 const routes: Routes = [
-    {path: 'products', component: ProductListComponent},
-    {path: 'welcome', component: WelcomeComponent},
-    {path: '', redirectTo: 'welcome', pathMatch:'full'},
-    {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/:id', component: ProductDetailComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
