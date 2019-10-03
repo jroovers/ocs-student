5ce445f2f3096bc75916cd9b3f57eba37d7143eb
Jeroen Roovers <jroovers93@gmail.com>
2019-10-03 14:21:26 +0200

    Exercise 10 finished: routing

--- workshop/app/app.module.ts
+++ workshop/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from "./app.component";
-import { ProductListComponent } from "./products/product-list.component";
 import { FormsModule } from "@angular/forms";
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { StarComponent } from './shared/star.component';
 import { HttpModule } from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
-import { WelcomeComponent } from './home/welcome.component';
+
+import { AppComponent } from "./app.component";
 import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductDetailGuard } from './products/product-guard.service';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+import { ProductListComponent } from "./products/product-list.component";
+import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   imports: [
@@ -25,6 +29,7 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ProductDetailComponent,
     StarComponent
   ],
+  providers: [ProductDetailGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
